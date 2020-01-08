4a72aaa37d6590d51cc2345ecef835e79321f945
octet-stream <nick.kruchinin@gmail.com>
2020-01-09 01:44:58 +0300

    Small fixes.

---
+++ .eslintrc.js
@@ -0,0 +1,100 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    browser: true
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    sourceType: "module",
+    allowImportExportEverywhere: false
+  },
+  plugins: [
+    "promise"
+  ],
+  extends: [
+    "airbnb-base"
+  ],
+  rules: {
+    camelcase: ["error"],
+    semi: ["error", "never"],
+    quotes: ["error", "double"],
+    indent: ["error", 2, {
+      SwitchCase: 1
+    }],
+    "max-len": [
+      "error", {
+        code: 80
+      }
+    ],
+    "linebreak-style": ["error", "unix"],
+    "eol-last": ["error", "unix"],
+    "require-yield": 0,
+    "no-unused-vars": ["warn"],
+    "no-console": 0,
+    "no-multi-spaces": ["error"],
+    "guard-for-in": ["error"],
+    "no-empty": ["error"],
+    "no-global-assign": ["error"],
+    "no-eval": ["error"],
+    "no-throw-literal": ["error"],
+    yoda: ["error"],
+    "no-with": ["error"],
+    "no-new-require": ["error"],
+    "no-tabs": ["error"],
+    "no-trailing-spaces": ["error"],
+    "no-inner-declarations": 0,
+    "no-class-assign": 0,
+    "no-unsafe-finally": ["error"],
+    "yield-star-spacing": ["error", "after"],
+    "object-curly-spacing": ["error", "never"],
+    "array-bracket-spacing": ["error", "never"],
+    "arrow-parens": ["error", "as-needed"],
+    "prefer-const": ["error", {
+      destructuring: "any",
+      ignoreReadBeforeAssign: false
+    }],
+    "no-restricted-syntax": ["error", "WithStatement"],
+    "no-unused-expressions": ["error", {
+      allowShortCircuit: true,
+      allowTernary: true
+    }],
+    "no-underscore-dangle": ["error", {
+      allowAfterThis: true,
+      allowAfterSuper: true
+    }],
+    "no-confusing-arrow": 0,
+    "no-await-in-loop": 0,
+    "class-methods-use-this": 0,
+    "no-param-reassign": 0,
+    "comma-dangle": 0,
+    "no-return-assign": 0,
+    "no-continue": 0,
+    "consistent-return": 0,
+    "global-require": 0,
+    "no-void": 0,
+
+    "func-names": ["error", "as-needed"],
+    "function-paren-newline": ["error", "consistent"],
+    "space-before-function-paren": ["error", {
+      asyncArrow: "always",
+      named: "never",
+      anonymous: "never"
+    }],
+    "prefer-destructuring": ["error", {
+      array: false,
+      object: false
+    }, {
+      enforceForRenamedProperties: false
+    }],
+    "object-curly-newline": ["error", {
+      consistent: true
+    }],
+
+    "import/no-extraneous-dependencies": 0,
+    "import/no-named-as-default-member": 0,
+    "import/no-dynamic-require": 0,
+    "import/extensions": 0,
+    "import/prefer-default-export": 0
+  }
+}
