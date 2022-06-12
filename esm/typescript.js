4ec1612bcc2cd1ffb81c652ba083a0a6c5f80d99
Nick K <nick.kruchinin@gmail.com>
2022-06-12 05:00:17 +0300

    Fix for esm typescript config.

--- esm/typescript.js
+++ esm/typescript.js
@@ -1,3 +1,8 @@
 module.exports = {
-  extends: ["./esm.js", "../typescript.js"]
+  extends: ["./esm.js", "../typescript.js"],
+  rules: {
+    // import plugin expects .ts extension for some readon
+    // so I forbid extensions this rule for .ts, .tsx
+    "import/extensions": ["error", "ignorePackages", {ts: "never", tsx: "never"}]
+  }
 }
