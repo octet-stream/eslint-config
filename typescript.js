0e8895c3f857e6c21b3d5952a83deae210c07f07
Nick K <nick.kruchinin@gmail.com>
2022-06-12 03:34:25 +0300

    Add typescript resolver

--- typescript.js
+++ typescript.js
@@ -2,6 +2,11 @@ module.exports = {
   extends: ["airbnb-typescript", "./base.js"],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
+  settings: {
+    "import/resolver": {
+      typescript: {}
+    }
+  },
   rules: {
     "@typescript-eslint/semi": 0,
     "@typescript-eslint/no-shadow": 0,
