1431935f56233d7394e8040ef72b48323ce67173
Nick K <nick.kruchinin@gmail.com>
2023-12-19 17:07:49 +0300

    Revert mark tests as cjs

--- ava.config.mjs
+++ ava.config.mjs
@@ -2,7 +2,7 @@ export default {
   failFast: true,
   workerThreads: false, // Disable to make tsx work
   extensions: {
-    ts: "commonjs"
+    ts: "module"
   },
   files: [
     "tests/**/*.test.ts"
