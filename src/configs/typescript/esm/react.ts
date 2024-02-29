382b4613f3eb1d41c6a2961e14e1ae122b177693
Nick K <nick.kruchinin@gmail.com>
2024-02-29 17:06:55 +0300

    fix(configs): Remove esm config from base typescript react config. Fix import/extensions for react ans ts configs.

--- src/configs/typescript/esm/react.ts
+++ src/configs/typescript/esm/react.ts
@@ -4,5 +4,16 @@ export default {
   extends: [
     "../esm.js",
     "../react.js"
-  ]
+  ],
+  rules: {
+    // Prohibit `.ts`, `.tsx`, `.mts`, and `.cts` extensions.
+    "import/extensions": ["error", "ignorePackages", {
+      ts: "never",
+      tsx: "never",
+      mts: "never",
+      cts: "never",
+      jsx: "never",
+      js: "ignorePackages"
+    }]
+  }
 } satisfies ESLint.ConfigData
